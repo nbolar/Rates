063dc0893c82bcd24333e2c9e2f027aa2bba8941
Nikhil Bolar <bolar.nikhil@gmail.com>
2019-04-03 15:22:58 +0300

    Initial Commit

---
+++ Rates/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rates
+//
+//  Created by Nikhil Bolar on 4/3/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

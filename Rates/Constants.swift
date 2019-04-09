6515f2d033a455aec1460f6c92906dcb05be1a79
Nikhil Bolar <45484873+nbolar@users.noreply.github.com>
2019-04-09 19:58:39 +0530

    Update Constants.swift

--- Rates/Constants.swift
+++ Rates/Constants.swift
@@ -13,7 +13,7 @@ typealias DownloadComplete = () -> ()
 
 let NOTIF_DOWNLOAD_COMPLETE = NSNotification.Name("dataDownloaded")
 
-var API_KEY = "9b954de2417aeb0f703b799680e7d4f4"
+var API_KEY = "YOUR_KEY"
 var API_URL = "http://data.fixer.io/api/latest?access_key=\(API_KEY)"
 var API_URL_FULL_FORM = "http://data.fixer.io/api/symbols?access_key=\(API_KEY)"
 var FULL_FORM = [String : String]()
